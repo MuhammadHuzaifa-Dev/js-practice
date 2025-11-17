003e04023825effd6ba7eb08c40be350b83a73e0
Muhammad Huzaifa <151584255+MuhammadHuzaifa-Dev@users.noreply.github.com>
2025-11-17 22:00:05 +0000

    learn datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all Js code as newer version 
+
+// alert(3*3) // we are using node.js not browser
+
+// number => 2 to the power of 53
+console.log(typeof 18);
+// string => ""
+console.log(typeof "huzaifa");
+// boolean => true/false
+console.log(typeof true);
+// null => standalone value
+console.log(typeof null); // output: object
+// undefined => 
+console.log(typeof undefined); // output: undefined
+// symbol => unique
+
+// object
