2da6aa9bb9c5cff4a031fa1cd6d98839fde72ff0
Muhammad Huzaifa <151584255+MuhammadHuzaifa-Dev@users.noreply.github.com>
2025-11-19 00:05:59 +0000

    Learn Conversion of Datatypes

---
+++ 01_basic/03_conversionOperations.js
@@ -0,0 +1,20 @@
+let score = "Huzaifa";
+// console.log(score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+let isLoggedIn = "Huzaifa";
+// console.log(isLoggedIn);
+// console.log(typeof isLoggedIn);
+
+let booleanStatus = Boolean(isLoggedIn);
+// console.log(booleanStatus);
+// console.log(typeof booleanStatus);
+
+let someNumber = 20;
+let stringStatus = String(someNumber);
+console.log(stringStatus);
+console.log(typeof stringStatus);
