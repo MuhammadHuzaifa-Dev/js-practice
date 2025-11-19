098133dcfa86af0fdd6b28f62dc9cf911c3951bd
Muhammad Huzaifa <151584255+MuhammadHuzaifa-Dev@users.noreply.github.com>
2025-11-19 20:22:04 +0000

    learn tricky coversion and preFix , profix

--- 01_basic/03_conversionOperations.js
+++ 01_basic/03_conversionOperations.js
@@ -16,5 +16,41 @@ let booleanStatus = Boolean(isLoggedIn);
 
 let someNumber = 20;
 let stringStatus = String(someNumber);
-console.log(stringStatus);
-console.log(typeof stringStatus);
+// console.log(stringStatus);
+// console.log(typeof stringStatus);
+
+//operations
+let value = 10;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let greet = "Assalamualikum";
+let name = " Huzaifa";
+
+let greetings = greet + name;
+// console.log(greetings);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1"+ 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(-"");
+
+//preFix
+let points = 10;
+let newPoints = points++
+// console.log(`newPoints ${newPoints}, points ${points}`);
+
+//postFix
+let gameCounter = 100;
+let newCounter = ++gameCounter
+// console.log(`newCounter ${newCounter}, gameCounter ${gameCounter}`);
