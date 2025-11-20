07d73f57dc0ec59050ab768bdb66dc81ec589c5c
Muhammad Huzaifa <151584255+MuhammadHuzaifa-Dev@users.noreply.github.com>
2025-11-20 23:11:26 +0000

    learn string  methods

---
+++ 01_basic/06_string.js
@@ -0,0 +1,23 @@
+let name = "Huzaifa";
+let repoCount = 11;
+
+// console.log(`My name is ${name} and repo count is ${repoCount}`);
+
+let myName = new String("huzaifa-dev");
+// console.log(myName[0]);
+// console.log(myName.__proto__);
+// console.log(myName.length);
+// console.log(myName.toUpperCase());
+// console.log(myName.charAt(2));
+// console.log(myName.indexOf("a"));
+// console.log(myName.substring(1,6));
+// console.log(myName.slice(-4,6));
+
+let newString = "   hello   ";
+// console.log(newString);
+// console.log(newString.trim());
+
+let url = "https://muhammadhuzaifa.dev/muhammad%20huzaifa";
+// console.log(url.replace('%20','-'));
+// 
+console.log(myName.split('-'));
