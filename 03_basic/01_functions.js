b482dc18d47bd6e3900c964c41b2c55f6ef28239
Muhammad Huzaifa <151584255+MuhammadHuzaifa-Dev@users.noreply.github.com>
2025-12-06 23:18:49 +0000

    learn complete functions in js

--- 03_basic/01_functions.js
+++ 03_basic/01_functions.js
@@ -25,4 +25,32 @@ function userLoggedIn(username = "User") {
     }
    return `Welcome, ${username}`
 }
-console.log(userLoggedIn())
+// console.log(userLoggedIn())
+
+function calculateCartPrice(val1,val2, ...num1) {
+    return num1
+}
+
+// console.log(calculateCartPrice(300,500,1300));
+
+const user = {
+    username:'huzaifa',
+    price:3000
+}
+
+function handleObject(anyObject) {
+    console.log(`course user is ${anyObject.username} and he paid ${anyObject.price}`);
+}
+
+// handleObject(user)
+// handleObject({
+//     username:"Muhammad",
+//     price:2500
+// })
+
+const newArray = [200,300,400]
+function getSecondArray(getArray) {
+    return getArray[1]
+}
+console.log(getSecondArray(newArray));
+console.log(getSecondArray([200,300,400]));
