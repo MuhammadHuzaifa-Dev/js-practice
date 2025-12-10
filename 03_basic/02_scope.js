9c7510af0473213d42586be7248a3f9d39a64528
Muhammad Huzaifa <151584255+MuhammadHuzaifa-Dev@users.noreply.github.com>
2025-12-10 20:55:26 +0000

    Learn Scope level and mini hoisting in Js

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -6,11 +6,45 @@
     let a = 10
     const b = 20
     var c = 30
-    console.log("let inner"+ a);
-    console.log("const inner"+ b);
-    console.log("var inner"+ c);
+    // console.log("let inner"+ a);
+    // console.log("const inner"+ b);
+    // console.log("var inner"+ c);
 }
 
-console.log("let outer"+ a);
-console.log("const outer"+ b);
-console.log("var outer"+ c);
+// console.log("let outer"+ a);
+// console.log("const outer"+ b);
+// console.log("var outer"+ c);
+
+
+function one() {
+    const username = "Huzaifa";
+    function two() {
+        const website = "www.vosprin.com"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+if(true){
+    const username = "Huzaifa";
+    if (username === "Huzaifa") {
+        const website = "vosprin.com"
+        // console.log(`${username} is founder of ${website}`);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// ++++++++++++++ Hoisting ++++++++++++++
+
+console.log(addOne(6))
+function addOne(num) {
+    return num + 1
+}
+
+console.log(addTwo(6));
+const addTwo = function(num){
+    return num + 2
+}
